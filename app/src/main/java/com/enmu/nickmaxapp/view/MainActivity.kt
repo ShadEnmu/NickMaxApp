9ae4ed0e21c09703e80e5249aae1b18c21f9eecd
ShadEnmu <111167441+ShadEnmu@users.noreply.github.com>
2022-09-09 01:32:12 +0800

    Добавлены меню для навигации и заготовка под первый фрагмент

---
+++ app/src/main/java/com/enmu/nickmaxapp/view/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.enmu.nickmaxapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.enmu.nickmaxapp.R
+import com.enmu.nickmaxapp.view.fragment.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val fragmentManager: FragmentManager = supportFragmentManager
+        fragmentManager.commit {
+            replace<HomeFragment>(R.id.fcvMainView)
+            setReorderingAllowed(true)
+            addToBackStack("Home")
+        }
+    }
+}
