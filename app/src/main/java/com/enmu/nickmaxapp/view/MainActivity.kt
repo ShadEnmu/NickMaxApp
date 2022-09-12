30d57023c1f6652272624a6730f3a9cffe395baf
ShadEnmu <111167441+ShadEnmu@users.noreply.github.com>
2022-09-12 10:57:49 +0800

    Navigation between fragments

--- app/src/main/java/com/enmu/nickmaxapp/view/MainActivity.kt
+++ app/src/main/java/com/enmu/nickmaxapp/view/MainActivity.kt
@@ -12,11 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fragmentManager: FragmentManager = supportFragmentManager
-        fragmentManager.commit {
-            replace<HomeFragment>(R.id.fcvMainView)
-            setReorderingAllowed(true)
-            addToBackStack("Home")
-        }
+
     }
 }
